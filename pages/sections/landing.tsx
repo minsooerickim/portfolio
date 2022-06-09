dcfcd8ccfb2f3a52de956af00902d70582d5b408
Minsoo Kim <minsooerickim@gmail.com>
2022-06-08 22:24:15 -0700

    navbar & cleanup

---
+++ pages/sections/landing.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import { useSession } from 'next-auth/react'
+
+import Header from '@/components/Header'
+
+export default function Home() {
+  const { data: session, status } = useSession()
+  const loading = status === "loading"
+
+  return (
+    <div>
+      <Head>
+        <title>Nextjs | Next-Auth</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main>
+        <div>
+           {loading && <div>Loading...</div>}
+           {
+            session &&
+              <>
+               <p style={{ marginBottom: '10px' }}> Welcome, {session.user!.name ?? session.user!.email}</p> <br />
+               <img src={session.user!.image!} alt="" />
+              </>
+            }
+           {
+            !session &&
+              <>
+               <img src="https://cdn.dribbble.com/users/759083/screenshots/6915953/2.gif" alt="" />
+               <p>GIF by <a href="https://dribbble.com/shots/6915953-Another-man-down/attachments/6915953-Another-man-down?mode=media">Another man</a> </p>
+              </>
+           }
+         </div>
+      </main>
+    </div>
+  )
+}
