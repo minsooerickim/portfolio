a07e3fef10b145b92ac70990f36d95e0a45714ee
Minsoo Kim <minsooerickim@gmail.com>
2022-06-08 21:07:32 -0700

    added paths to tsconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,45 +2,14 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import { useSession } from 'next-auth/react'
 
+import Landing from '@/pages/sections/landing'
+
 export default function Home() {
   const { data: session, status } = useSession()
-  const loading = status === "loading"
 
   return (
-    <div>
-      <Head>
-        <title>Nextjs | Next-Auth</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <main>
-        <h1>Authentication in Next.js app using Next-Auth</h1>
-        <div>
-           {loading && <div>Loading...</div>}
-           {
-            session &&
-              <>
-               <p style={{ marginBottom: '10px' }}> Welcome, {session.user!.name ?? session.user!.email}</p> <br />
-               <img src={session.user!.image!} alt="" />
-              </>
-            }
-           {
-            !session &&
-              <>
-               <p>Please Sign in</p>
-               <img src="https://cdn.dribbble.com/users/759083/screenshots/6915953/2.gif" alt="" />
-               <p>GIF by <a href="https://dribbble.com/shots/6915953-Another-man-down/attachments/6915953-Another-man-down?mode=media">Another man</a> </p>
-              </>
-           }
-         </div>
-      </main>
-      <div>
-        <div>Next + Tailwind ❤️</div>
-        <p>
-          Next and Tailwind CSS are a match made in heaven, check out this article on how
-          you can combine these two for your next app.
-        </p>
-      </div>
-    </div>
+    <span className='flex justify-center px-4 w-full bg-pattern bg-repeat bg-contain'>
+      <Landing/>
+    </span>
   )
 }
