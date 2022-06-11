7d481e26f6aca7714ec54f327e94abbce2f6c637
Minsoo Kim <minsooerickim@gmail.com>
2022-06-10 19:20:57 -0700

    layout fix

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -1,18 +1,16 @@
 import { Page } from '@/components/Page/Page';
 import CurrentProjects from '@/components/Project/CurrentProjects';
 import PastProjects from '@/components/Project/PastProjects';
-import { GetStaticProps, InferGetServerSidePropsType, InferGetStaticPropsType } from 'next'
-import { Key, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from 'react'
-import getProjects from './api/projects/current';
+import { motion } from 'framer-motion';
+import { InferGetStaticPropsType } from 'next'
 
 export default function Projects({ currentProjects, pastProjects }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Page title='Projects'>
-      <div className="flex flex-col space-y-4">
+      <motion.div layout className="grid grid-cols-2 gap-20">
         <CurrentProjects currentProjects={currentProjects} pastProjects={undefined} />
-          <h2>Past</h2>
-          <PastProjects pastProjects={pastProjects} currentProjects={undefined} />
-      </div>
+        <PastProjects pastProjects={pastProjects} currentProjects={undefined} />
+      </motion.div>
     </Page>
   )
 }
