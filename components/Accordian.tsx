01911ca9fe97bfee3eb18543c5a9b9388e225726
Minsoo Kim <minsooerickim@gmail.com>
2022-06-12 23:41:26 -0700

    profile card + accordion

---
+++ components/Accordian.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import { QuestionIcon } from './Icons';
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  // border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+  ))(({ theme }) => ({
+    // backgroundColor: 'bg-background',
+    flexDirection: 'row-reverse',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+      transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+      marginLeft: theme.spacing(1),
+    },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  // backgroundColor: 'bg-subBackground'
+  // borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
+
+export default function CustomizedAccordions() {
+  const [expanded, setExpanded] = React.useState<string | false>('panel1');
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
+
+  return (
+    <div className='flex justify-center items-center'>
+        <div className='space-y-2 w-6/12'>
+            <Accordion sx={{ '& .MuiSvgIcon-root.MuiSvgIcon-fontSizeMedium': { color: '#7de2d1' } }} className='shadow-md rounded-lg bg-card' expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                <Typography className='text-normalText font-bold'>What tools do I use?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography className='text-secondaryNormalText'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion sx={{ '& .MuiSvgIcon-root.MuiSvgIcon-fontSizeMedium': { color: '#7de2d1' } }} className='shadow-md rounded-lg bg-card' expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
+                <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+                <Typography className='text-normalText font-bold'>Favorite Project?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography className='text-secondaryNormalText'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion sx={{ '& .MuiSvgIcon-root.MuiSvgIcon-fontSizeMedium': { color: '#7de2d1' } }} className='shadow-md rounded-lg bg-card' expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+                <Typography className='text-normalText font-bold'>Think of another question...</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography className='text-secondaryNormalText'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
+                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    </div>
+  );
+}
---
+++ components/ProfileCard.tsx
@@ -0,0 +1,31 @@
+import { useSession } from "next-auth/react"
+
+export default function Landing() {
+    const { data: session, status } = useSession()
+    const loading = status === "loading"
+  
+    return(
+        <div className="flex justify-center">
+            <div className="w-6/12 rounded-3xl overflow-hidden shadow-xl bg-card ">
+                <img src="/assets/Colored-Shapes.svg" className="w-full"/>
+                <div className="flex justify-center -mt-8">
+                    <img src="https://i.imgur.com/y72AJR2.png" className="w-6/12 rounded-full border-solid  border-2 -mt-3"/>
+                </div>
+                <div className="text-center px-3 pb-6 pt-2">
+                    <p className="mt-2 text-md font-bold text-normalText">CS @ UC Riverside</p>
+                    <p className="mt-2 font-light text-secondaryNormalText">Feel free to reach out to me at minsooerickim@gmail.com</p>
+                </div>
+                <div className="flex justify-center pb-3">
+                <div className="text-center mr-3 border-r pr-3">
+                    <h2 className="text-text">4</h2>
+                    <span className="text-text">Projects</span>
+                </div>
+                <div className="text-center">
+                    <h2 className="text-text">20</h2>
+                    <span className="text-text">Tools</span>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
