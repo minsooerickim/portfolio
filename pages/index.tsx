09a62f75c30ccfeb177569e88d8f9bdba372c57f
Minsoo Kim <minsooerickim@gmail.com>
2022-06-12 23:41:03 -0700

    speedial in progress in landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,16 @@ import { useSession } from 'next-auth/react'
 
 import Landing from '@/pages/sections/landing'
 import { Page } from '@/components/Page/Page'
+import SpeedDial from '@/components/SpeedDial'
 
 export default function Home() {
   const { data: session, status } = useSession()
 
   return (
-    <span className='flex justify-center px-4 w-full bg-pattern bg-repeat bg-contain'>
+    <span className='flex px-4 w-full bg-pattern bg-repeat bg-contain'>
       <Page title='Home'>
           <Landing />
+          {/* <SpeedDial /> */}
       </Page>
     </span>
   )
