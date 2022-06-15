4fa8a306d17a4bafd8c5312815ea089811ef2a5d
Minsoo Kim <minsooerickim@gmail.com>
2022-06-15 01:46:38 -0700

    in progress fix speeddial colors, links

--- components/SpeedDial.tsx
+++ components/SpeedDial.tsx
@@ -7,35 +7,43 @@ import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import PrintIcon from '@mui/icons-material/Print';
 import ShareIcon from '@mui/icons-material/Share';
+import Router from 'next/router'
 
 const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
+  { icon: <FileCopyIcon />, name: 'Resume' },
+  { icon: <SaveIcon />, name: 'LinkedIn' },
+  { icon: <PrintIcon />, name: 'GitHub' },
+  { icon: <ShareIcon />, name: 'Email' },
 ];
 
 export default function BasicSpeedDial() {
+  const handleClick = (e: { preventDefault: () => void; }, name) => {
+    console.log(name)
+    if (name=="Resume"){
+
+    } else if(name=="LinkedIn"){
+      window.open ('https://www.linkedin.com/in/minsookime/', '_ blank')
+    } else if(name=="GitHub"){
+      window.open ('https://github.com/minsooerickim', '_ blank')
+    } else if(name=="Email"){
+      window.open ('mailto:minsooerickim@gmail.com') // make it not open a new tab
+    }
+  }
   return (
     <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
       <SpeedDial
-        ariaLabel="SpeedDial basic example"
+        ariaLabel="SpeedDial"
         sx={{ position: 'absolute', bottom: 16, right: 16 }}
-        icon={<SpeedDialIcon />}
-        FabProps={{
-          sx: {
-            bgcolor: '#339989',
-            '&:hover': {
-              bgcolor: '#278274',
-            }
-          }
-        }}
+        icon={<SpeedDialIcon sx={{'& .MuiSvgIcon-root': {color: '#339989'}}}/>}
       >
         {actions.map((action) => (
           <SpeedDialAction
             key={action.name}
             icon={action.icon}
             tooltipTitle={action.name}
+            onClick={(e) => {
+              handleClick(e, action.name)
+            }}
           />
         ))}
       </SpeedDial>
