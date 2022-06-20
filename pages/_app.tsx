3ffdd9491cccacf1bdceb087ae4957066f668098
Minsoo Kim <minsooerickim@gmail.com>
2022-06-19 21:32:34 -0700

    mobile nav fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,10 +30,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           <div className="fixed right-0 bottom-0 pb-14 md:p-4 z-10">
             <SpeedDial />
           </div>
-          <div className="flex md:hidden justify-center sticky bottom-0 pb-2">
+          <div className="fixed bottom-0 z-10  translate-x-2/4 pb-4">
             <MobileNav />
           </div>
-          {/* <Footer /> */}
         </ThemeProvider>
       </SessionProvider>
     </>
