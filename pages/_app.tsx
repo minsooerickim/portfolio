3c4380a7bdc403397e7697d2e8440ecd84cf16c4
Minsoo Kim <minsooerickim@gmail.com>
2022-06-19 21:34:23 -0700

    hide mobile nav on big screens

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,7 +30,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <div className="fixed right-0 bottom-0 pb-14 md:p-4 z-10">
             <SpeedDial />
           </div>
-          <div className="fixed bottom-0 z-10  translate-x-2/4 pb-4">
+          <div className="fixed md:hidden bottom-0 z-10  translate-x-2/4 pb-4">
             <MobileNav />
           </div>
         </ThemeProvider>
